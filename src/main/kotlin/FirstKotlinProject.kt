2dd5b1aba737ef4ac5fb3eb537584cc1792e9bba
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-02 08:59:41 -0400

    3rd commit

--- src/main/kotlin/FirstKotlinProject.kt
+++ src/main/kotlin/FirstKotlinProject.kt
@@ -5,10 +5,14 @@ Author: Aziza San Buenaventura
 Date Created: September 1, 2022
 File name: FirstKotlinProject.kt
  */
-    println("Hello World!")
-print ("Hello World!!!!")
-    print("Hello World!!!!")
-    println("Hello World4")
-    print("Hello World5")
+   println("Enter your number 1:")
+    var number1: Int = readln(). toInt ()
+
+    println("Enter your number 2:")
+    var number2: Int = readln(). toInt ()
+
+    var sum: Int = number1 + number2
+    println("The sum is $sum")
+
 
 }
