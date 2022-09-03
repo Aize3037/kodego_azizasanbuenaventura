a90eb8d8bd9a6255eb060620ae37b898696c0e04
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-03 06:53:59 -0400

    Activit2

---
+++ src/main/kotlin/practice_activities/Activity2.kt
@@ -0,0 +1,13 @@
+package practice_activities
+fun main(){
+    println("Enter Grade in Math")
+    var grade= readln().toInt()
+    println("Enter Grade in English")
+    var grade1= readln().toInt()
+    println("Enter Grade in Filipino")
+    var grade2= readln().toInt()
+var number1 : Int =257
+    var number2 : Int = 3
+    var average : Int = 257 / 3
+    println("average is $average")
+}
