75bc1b677ea22ac34d044013dbd4a97aeeb7454a
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-03 00:44:09 -0400

    Fist commit

---
+++ src/main/kotlin/exercises/Activity01-A.kt
@@ -0,0 +1,17 @@
+package exercises
+fun main() {
+
+    /*
+Author: Aziza San Buenaventura
+Date Created: September 3, 2022
+File name: Activity 01 - A
+ */
+println("Directory: The Pioneer")
+    var student1: String = "Aziza"
+    println("Enter your name:")
+var name = readln()
+    println("Progress:")
+    var progress: String = "Incomplete"
+    println("Incomplete")
+
+}
