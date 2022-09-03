917450b889c53a00701b7f15c7f8b413e7588722
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-03 05:47:58 -0400

    Activity1

---
+++ src/main/kotlin/practice_activities/Activity1.kt
@@ -0,0 +1,15 @@
+package practice_activities
+fun main(){
+    println("Enter Your First Name:")
+    var name = readln()
+    println(name)
+    println("Enter Your Middle Name/Initial")
+    var initial = readln()
+    println(initial)
+    println("Enter Your Last Name")
+    var lastname= readln()
+    println(lastname)
+    val name1: String = "Aziza V. San Buenaventura"
+    println(name1)
+    println("Your Name is $name1")
+}
