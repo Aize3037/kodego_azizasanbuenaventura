832f08e2aa3ec175be40e3cbafdd627d0d017e1b
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-07 07:55:39 -0400

    KotlinProgram13

---
+++ src/main/kotlin/KotlinProgram13.kt
@@ -0,0 +1,13 @@
+fun main(){
+    //
+    //array size = 3
+    var names = arrayOf("john","paul","Mark")
+            println(names[1])
+
+    //explicit
+    var numbers = arrayOf<Int>(2,3,10,12,9)
+    println(numbers[4])
+
+    //array size
+    println(numbers.size)
+}
