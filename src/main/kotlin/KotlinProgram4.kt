36973d36d5c21f146586c84df542670f5a6b94d8
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-10 14:15:32 -0400

    KotlinProgram4

---
+++ src/main/kotlin/KotlinProgram4.kt
@@ -0,0 +1,21 @@
+fun main(){
+    var number : Int = 10
+
+    if(number < 10) {
+        println("Hello")
+    }
+
+    var grades : Int = 75
+    if(grades >= 75){
+        println("you passed!")
+    }
+
+ // range
+    var idNumber: Int = 1
+    // true
+    if((idNumber >=1)&&(idNumber <=3) ){
+        println("valid id number")
+        println("Success!!!!")
+    }
+    println("Line ends")
+}
