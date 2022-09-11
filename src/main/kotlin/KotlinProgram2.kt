4537fda78cf24ec4c3edd36750f342336e017a6b
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-11 16:42:40 -0400

    KotlinProgram2

---
+++ src/main/kotlin/KotlinProgram2.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var gender : String = "X"
+    if(gender == "M"){
+        println("Male")
+    }else if(gender == "F"){
+        println("Female")
+    }else{
+        println("Invalid choise")
+    }
+}
