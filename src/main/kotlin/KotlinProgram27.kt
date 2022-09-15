f42e6a29ca6beed72b111e419892b641c872ace7
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-15 07:55:35 -0400

    2nd commit

--- src/main/kotlin/KotlinProgram27.kt
+++ src/main/kotlin/KotlinProgram27.kt
@@ -1,21 +1,19 @@
 fun main(){
-    var joe = Person("Joe", 21) // instantiation
-    println("********")
-    println("${joe.name}'s current age is ${joe.age}")
-    println("After 1 year....")
-    joe.ageUp()
-    println("${joe.name}'s current age is ${joe.age}")
-
-
+    var anne = Person ("Anne")
+    anne.age = 25
+    anne.displayAge()
 }
 
-class Person(var name: String, var age: Int) { // constructor
+class Person(var name: String, var age: Int) { // constructor -> primary constructor
 
     init{
         println("Object Created")
         println("name is $name")
         println("age is $age")
     }
+    // secondary constructor
+    constructor(name:String):this(name, 0){}
+    constructor(age:Int):this("default_name",age){}
     fun walk(){
         println("A person walks")
     }
@@ -23,6 +21,7 @@ class Person(var name: String, var age: Int) { // constructor
         println("A person eats")
     }
     fun displayAge(){
+        //var age: Int = 7
         println(age)
     }
     fun ageUp(){
