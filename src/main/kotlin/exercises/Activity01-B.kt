587a84ba7a4fea23bfa51c1caebf46310aa7f6a8
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-18 20:50:52 -0400

    Activity01-B

---
+++ src/main/kotlin/exercises/Activity01-B.kt
@@ -0,0 +1,22 @@
+package exercises
+
+var book1: String = "Math Book"
+var characteristics1: String = "Educational"
+var book2: String = "Romantic Novel"
+var characteristics2: String = "Fictional"
+var book3: String = "World War 2"
+var characteristics3: String = "History"
+var book4: String = "The World Almanac"
+var characteristics4: String = "Almanac"
+
+fun main() {
+    println("Available Books:")
+    println(characteristics1)
+    println(book1)
+    println(characteristics2)
+    println(book2)
+    println(characteristics3)
+    println(book3)
+    println(characteristics4)
+    println(book4)
+}
