0398bc5c27d00c56091987029d2e784a0ee7721d
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-20 08:42:40 -0400

    KotlinProgram31

---
+++ src/main/kotlin/KotlinProgram31.kt
@@ -0,0 +1,9 @@
+fun main(){
+    try{
+        println("Enter number 1:")
+        var num1: Int = readln().toInt()
+   }catch(e:Exception){
+        println("Invalid input Digital only!")
+  }
+    println("-End of Code-")
+}
