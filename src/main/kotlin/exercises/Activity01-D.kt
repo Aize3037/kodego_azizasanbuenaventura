766c5c66557412806c3a3264e40e4035ed83032a
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-20 08:19:48 -0400

    Activity01-D

--- src/main/kotlin/exercises/Activity01-D.kt
+++ src/main/kotlin/exercises/Activity01-D.kt
@@ -5,10 +5,16 @@ fun main() {
     var amount1: Int = readln().toInt()
     println("Enter another amount:")
     var amount2: Int = readln().toInt()
+    var total: Int = amount1+amount2
+
+    println("Total is $total")
     println("Do you want to add another amount?")
+
     var answer: String = readln()
        if (answer == "No")
            println("Divide the value by how many?")
+    var divisor: Int = readln().toInt()
+    println("The answer is ${total / divisor}")
     }
 
 
