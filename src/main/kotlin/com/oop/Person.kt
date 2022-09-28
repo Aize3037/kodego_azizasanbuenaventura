48a52fc6412302e8bb47f5ae218548a507bdcffb
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-28 11:20:28 -0400

    Person.kt

--- src/main/kotlin/com/oop/Person.kt
+++ src/main/kotlin/com/oop/Person.kt
@@ -1,5 +1,6 @@
 package com.oop
-
+//Abstraction // encapsulation
+//inheritance -> *is - a relationship
 open class Person (name:String, age: Int) {
     var name: String = name
     var age: Int = age
@@ -9,12 +10,19 @@ open class Person (name:String, age: Int) {
         println("Object Created!")
         println("name is $name, age is $age")
     }
-
+    //polymorphism
+    //1) overriding - same name, same parameters
+    // different Implementation, one is in the superclass,
+    // one is in the subclass
 
     open fun displayName(){
         println("Name is $name")
     }
+    // 2) overloading - same name, different parameters, same class
 
+    fun displayName(name: String){
+        println("Name is $name")
+    }
     fun displayName(numberOfTimes: Int){
         var ctr = numberOfTimes
         while(ctr >=0){
