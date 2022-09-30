c77faa4dfb91238f86607ca98433d4ab9b91ed4f
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-30 06:13:29 -0400

    Activity01-A

--- src/main/kotlin/exercises/Activity01-A.kt
+++ src/main/kotlin/exercises/Activity01-A.kt
@@ -1,6 +1,31 @@
 package exercises
 
 fun main(){
-var studentName: String = ""
-studentName
+    println("School Directory:")
+
+    var studentName1: String = "John Smith"
+    println(studentName1)
+    var age1: Int = 21
+    println(age1)
+    var cityAddress1: String = "Pasig City"
+    println(cityAddress1)
+    println("-----------------")
+    var studentName2: String = "David Lee"
+    println(studentName2)
+    var age2: Int = 22
+    println(age2)
+    var cityAddress2: String = "Makati City"
+    println(cityAddress2)
+    println("------------------")
+    println("John Smith's Grade")
+    var subject1: String = "Math"
+    var score1: Int = 90
+    println(subject1)
+    println(score1)
+    println("David Lee's Grade")
+    var subject2: String = "Math"
+    var score2: Int = 85
+    println(subject2)
+    println(score2)
+    println("----End----")
 }
