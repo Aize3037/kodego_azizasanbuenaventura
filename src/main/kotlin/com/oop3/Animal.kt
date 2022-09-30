4873fc172758b2f6dbb4b1c8a9aca818b419ef3e
aziza sanbuenaventura <appledrones003@gmail.com>
2022-09-30 08:31:44 -0400

    oop3

---
+++ src/main/kotlin/com/oop3/Animal.kt
@@ -0,0 +1,9 @@
+package com.oop3
+//1. not a class, no construction
+//2. cannot create an object
+//3. can have abstract functions without abstract keyword
+//4. being implemented rather than inherited
+interface Animal {
+    fun walk()
+    fun makeSound()
+}
---
+++ src/main/kotlin/com/oop3/Dog.kt
@@ -0,0 +1,17 @@
+package com.oop3
+
+class Dog:Mammal(),Animal,Pet {
+    override fun walk() {
+     println("Dog walks")
+    }
+
+    override fun makeSound() {
+       println("aw aw")
+    }
+    override fun groom(){
+        println("Dog can be groomed")
+    }
+    override fun feed(){
+        println("Dog Feeds")
+    }
+}
