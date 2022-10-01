0a83ddbe8da4e7052b71a9cd1a9eabd21d668162
aziza sanbuenaventura <appledrones003@gmail.com>
2022-10-01 00:29:45 -0400

    Activity01-E

--- src/main/kotlin/exercises/Activity01-E.kt
+++ src/main/kotlin/exercises/Activity01-E.kt
@@ -1,2 +1,21 @@
 package exercises
 
+fun main() {
+
+  var setA = mutableSetOf<Char>()
+  println("Enter String 1:")
+  var string1: String = readln()
+  for (letter in string1){
+    setA.add(letter)
+  }
+
+  var setB = mutableSetOf<Char>()
+  println("Enter String 2:")
+  var string2: String = readln()
+  for(letter in string2){
+    setB.add(letter)
+  }
+
+  println("The unique in set A is ${setA.minus(setB)}")
+  println("The unique in set B is ${setB.minus(setA)}")
+}
