1298aa4db3e7efb8e4084b6625cd825274ed67de
aziza sanbuenaventura <appledrones003@gmail.com>
2022-10-01 01:54:47 -0400

    Activity01-F

--- src/main/kotlin/exercises/Activity01-F.kt
+++ src/main/kotlin/exercises/Activity01-F.kt
@@ -1,19 +1,20 @@
 package exercises
 fun main() {
-    println("Enter a word:")
-    var originalString1 = "madam"
-    readln()
-
-    var reverseString = originalString1.reversed()
-
-    if (originalString1 == reverseString) {
-        println("The given string is Palindrome")
-    } else {
-        println("The given string is NOT a Palindrome")
-
-    }
+  println("Word: \"madam\"")
+  println("isPalindrome : ${isPalindrome("madam")}")
+  println("---------------------------")
+  println("Word: \"jello\"")
+  println("isPalindrome : ${isPalindrome("jello")}")
 }
-
+    fun isPalindrome(text:String): Boolean {
+     //   val textLen = text.length
+    //    println(text)
+     //   if(text.length < 2) {
+     //       return true
+        return text == text.reversed()
+    }
+      //  return text.first() == text.last()
+      //          && isPalindrome(text.substring(1,textLen-1))
 
 
 
