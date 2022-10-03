124ce1b2a46693f69d167b6303f7997f4dd9f1dc
aziza sanbuenaventura <appledrones003@gmail.com>
2022-10-02 23:49:00 -0400

    KotlinProgram29.kt

--- src/main/kotlin/KotlinProgram29.kt
+++ src/main/kotlin/KotlinProgram29.kt
@@ -3,15 +3,15 @@ fun main(){
 
 }
 
-class Empty {}
+class Empty {} // Usually used as placeholder
 
 
 class outer {
 
     val name = "Outer"
-    fun show() = "the name: $name"
+    fun show() = "The name: $name"
 
-    class Nested {
+    class Nested { // No access to function in the class where it belongs
         val name = "Nested"
         fun show() = "the name: $name"
 
@@ -23,10 +23,15 @@ class Outer2 {
     fun show() = "the name: $name1"
 
     inner class Inner {
-        val name2 = "Inner"
+        val name2 = "Inner" // Has access to functions in the class where it belongs
         fun show() = "data: $name2 and $name1"
     }
 }
+
+// sealed class
+// main class
+// abstract class
+// data class
 
 // sealed class
 // open class
---
+++ src/main/kotlin/com/oop4/Books.kt
@@ -0,0 +1,4 @@
+package com.oop4
+
+class Books {
+}
