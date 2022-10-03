e3c892daa8e704aec0bdc4ef9e57ebd83c43ebae
aziza sanbuenaventura <appledrones003@gmail.com>
2022-10-03 07:37:12 -0400

    Activity01-G

--- src/main/kotlin/exercises/Activity01-G.kt
+++ src/main/kotlin/exercises/Activity01-G.kt
@@ -1,17 +1,23 @@
 package exercises
 
 fun main () {
- //var array = intArrayOf(2,3,4,5)
- //for(element in array){
- //    print(element)
-// }
- var number1: IntArray = intArrayOf(2,3,4,5)
- for (i in number1.indices){
-       println(number1[i])
- }
-    println("--------------")
-    var number2: IntArray = intArrayOf(6,7,8,9)
-    for (i in number2.indices){
+    var number1: IntArray = intArrayOf(2, 3, 4, 5)
+    for (i in number1.indices) {
+        println(number1[i])
+
+    }
+
+    var number2: IntArray = intArrayOf(6, 7, 8, 9)
+    for (i in number2.indices) {
         println(number2[i])
-  }
+    }
+        println("Lower values:")
+        println(number1.min())
+        println(number2.min())
+        println("Higher Values:")
+        println(number1.max())
+        println(number2.max())
+
 }
+
+
