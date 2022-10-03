c150b07c6b7f6b37612272d5788ec6d21ce3d669
aziza sanbuenaventura <appledrones003@gmail.com>
2022-10-03 07:15:05 -0400

    Main.kt

--- src/main/kotlin/com/oop4/Main.kt
+++ src/main/kotlin/com/oop4/Main.kt
@@ -1,2 +1,29 @@
 package com.oop4
 
+fun main(){
+    println("Welcome to the library:")
+    println("Here's the available books!")
+    println("----------")
+    println("Available Books:")
+    var bookListed = Books()
+    bookListed.showBooks()
+    println("-----------")
+    println("Genre:")
+    bookListed.showGenre()
+    println("-----------")
+    println("Type of Books:")
+    bookListed.showType()
+
+    println("Do you want to borrow books?")
+    var answer : String = readln().lowercase()
+    if (answer == "yes") {
+        println("Enter the book you want to borrow:")
+        readln()
+        println("Thank you for borrowing!")
+    }else if(answer == "no"){
+        println("Have a nice day!")
+    }else{
+        println("Invalid Input")
+    }
+}
+
