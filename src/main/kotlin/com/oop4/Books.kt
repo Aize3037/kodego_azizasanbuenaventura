7df02208e6b61e5b96cc31840c37dcf44832a523
aziza sanbuenaventura <appledrones003@gmail.com>
2022-10-03 07:16:53 -0400

    Books.kt

--- src/main/kotlin/com/oop4/Books.kt
+++ src/main/kotlin/com/oop4/Books.kt
@@ -1,4 +1,25 @@
 package com.oop4
 
-class Books {
+open class Books() {
+    var books = arrayListOf("a.Dracula", "b.The Little Mermaid", "c.World Almanac")
+    var genre = arrayListOf("a.Horror", "b.Fantasy", "c.History")
+    var type = arrayListOf("a.Fictional", "b.Children's book", "c.Educational")
+
+  open fun showBooks() {
+        for (books in books) {
+            println(books)
+        }
+    }
+   open fun showGenre(){
+       for(genre in genre){
+           println(genre)
+       }
+   }
+    open fun showType(){
+       for(type in type){
+           println(type)
+       }
+   }
 }
+
+
--- src/main/kotlin/exercises/Activity01-G.kt
+++ src/main/kotlin/exercises/Activity01-G.kt
@@ -1,2 +1,17 @@
 package exercises
 
+fun main () {
+ //var array = intArrayOf(2,3,4,5)
+ //for(element in array){
+ //    print(element)
+// }
+ var number1: IntArray = intArrayOf(2,3,4,5)
+ for (i in number1.indices){
+       println(number1[i])
+ }
+    println("--------------")
+    var number2: IntArray = intArrayOf(6,7,8,9)
+    for (i in number2.indices){
+        println(number2[i])
+  }
+}
